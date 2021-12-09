bd59c43dc6452390712ad45b98d7e2d3fb23f9c6
derech1e <jojopaul88@gmail.com>
2021-12-09 22:25:44 +0100

    Added Extension Class

--- Logbook/CircleLoader.swift
+++ Logbook/CircleLoader.swift
@@ -84,15 +84,3 @@ struct CircleLoader_Previews: PreviewProvider {
         CircleLoader()
     }
 }
-
-extension Color {
-    static let circleTrackStart: Color = Color(r: 237, g: 242, b: 255)
-    static let circleTrackEnd: Color = Color(r: 235, g: 248, b: 255)
-    
-    static let circleRoundStart: Color = Color(r: 71, g: 198, b: 255)
-    static let circleRoundEnd: Color = Color(r: 90, g: 131, b: 255)
-    
-    init(r: Double, g: Double, b: Double) {
-        self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0)
-    }
-}
