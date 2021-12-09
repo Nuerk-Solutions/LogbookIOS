8e4c86de8d024e108797eed9da815c232552b44e
derech1e <jojopaul88@gmail.com>
2021-12-09 20:36:15 +0100

    Added status Code exception handling

--- Logbook/Data.swift
+++ Logbook/Data.swift
@@ -22,8 +22,8 @@ struct Vehicle: Codable, Identifiable {
     var typ: VehicleEnum
     var currentMileAge: Int
     var newMileAge: Int
-//    var distance: Int
-//    var cost: Float
+    //    var distance: Int
+    //    var cost: Float
 }
 
 struct AdditionalInformation: Codable, Identifiable {
@@ -35,27 +35,46 @@ struct AdditionalInformation: Codable, Identifiable {
 }
 
 class Api {
-    func getLogbookEntries(completion: @escaping ([Logbook]) -> ()) {
-        guard let url = URL(string: "https://api.nuerk-solutions.de/logbook") else { return } // guard adds a condition. because swift is typesafe you need to be sure to have the right type; if the URL is empty or nil then return the func at that point
+    func getLogbookEntries(completion: @escaping ([Logbook]?, Error?) -> ()) {
+        guard let url = URL(string: "https://api.nuerk-solutions.de/logbook") else { fatalError("Missing URL")} // guard adds a condition. because swift is typesafe you need to be sure to have the right type; if the URL is empty or nil then return the func at that point
         
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            if let data = data {
-                let decoder = JSONDecoder()
-                decoder.dateDecodingStrategyFormatters = [DateFormatter.standardT]
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let logbookEntries = try! decoder.decode([Logbook].self, from: data) // json decode
-                DispatchQueue.main.async {
-                    completion(logbookEntries)
-                }
-                print("Fetch in API Sucessfully")
-            }
-        }
-        .resume()
+        let urlRequest = URLRequest(url: url)
+        
+        
+        let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+             if let error = error {
+                 print("Request error: ", error)
+                 return
+             }
+
+             guard let response = response as? HTTPURLResponse else { return }
+
+             if response.statusCode == 200 {
+                 guard let data = data else { return }
+                 
+                 let decoder = JSONDecoder()
+                 decoder.dateDecodingStrategyFormatters = [DateFormatter.standardT]
+                 decoder.keyDecodingStrategy = .convertFromSnakeCase
+                 
+                 DispatchQueue.main.async {
+                     do {
+                         let logbookEntries = try decoder.decode([Logbook].self, from: data)
+                         completion(logbookEntries, nil)
+                     } catch let error {
+                         print("Error decoding: ", error)
+                     }
+                 }
+             } else {
+                 completion(nil, HttpError.statusNot200)
+             }
+         }
+
+         dataTask.resume()
     }
 }
 
 extension JSONDecoder {
-
+    
     /// Assign multiple DateFormatter to dateDecodingStrategy
     ///
     /// Usage :
@@ -71,16 +90,16 @@ extension JSONDecoder {
         set {
             guard let formatters = newValue else { return }
             self.dateDecodingStrategy = .custom { decoder in
-
+                
                 let container = try decoder.singleValueContainer()
                 let dateString = try container.decode(String.self)
-
+                
                 for formatter in formatters {
                     if let date = formatter.date(from: dateString) {
                         return date
                     }
                 }
-
+                
                 throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot decode date string \(dateString)")
             }
         }
@@ -94,13 +113,13 @@ extension DateFormatter {
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
         return dateFormatter
     }()
-
+    
     static let standard: DateFormatter = {
         var dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         return dateFormatter
     }()
-
+    
     static let yearMonthDay: DateFormatter = {
         var dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
