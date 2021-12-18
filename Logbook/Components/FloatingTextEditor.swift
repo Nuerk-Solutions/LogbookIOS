31e4a5461e7ce8057ee23259f0a054b0db69dc1a
derech1e <jojopaul88@gmail.com>
2021-12-19 00:23:49 +0100

    Added Floating TextEditor

---
+++ Logbook/Components/FloatingTextEditor.swift
@@ -0,0 +1,33 @@
+//
+//  FloatingTextEditor.swift
+//  Logbook
+//
+//  Created by Thomas on 18.12.21.
+//
+
+import SwiftUI
+
+struct FloatingTextEditor: View {
+    let title: String
+    let text: Binding<String>
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Text(title)
+                .foregroundColor(text.wrappedValue.isEmpty ? Color(.placeholderText) : .accentColor)
+                .offset(x: 1, y: text.wrappedValue.isEmpty ? 0 : -25 - CGFloat((text.wrappedValue.numberOfLines)-1) * 15)
+                .scaleEffect(text.wrappedValue.isEmpty ? 1 : 0.75, anchor: .leading)
+            TextEditor(text: text)
+        }
+        .padding(.top, 15)
+        .padding(.bottom, 5)
+        .animation(.easeOut(duration: 0.1))
+    }
+}
+
+struct FloatingTextEditor_Previews: PreviewProvider {
+    @State private static var text = "TestContent"
+    static var previews: some View {
+        FloatingTextEditor(title: "Hellow World", text: $text)
+    }
+}
