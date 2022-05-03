352b67dac380d2307b329be948fac054b6fb3077
derech1e <jojopaul88@gmail.com>
2022-05-03 19:35:37 +0200

    Added URL deeplink/ urlscheme

---
+++ Logbook/Extensions/UrlExtension.swift
@@ -0,0 +1,36 @@
+//
+//  UrlExtension.swift
+//  Logbook
+//
+//  Created by Thomas on 03.05.22.
+//
+
+import Foundation
+
+extension URL {
+    var isDepplink: Bool {
+        return scheme == "logbookapp"
+    }
+    
+    var driverIdentifier: DriverEnum? {
+        guard isDepplink else { return nil }
+        
+        return DriverEnum(rawValue: host ?? "")
+    }
+    
+    var startDateIdentifier: Date? {
+        guard pathComponents.count > 1, let date = DateFormatter.yearMonthDay.date(from: pathComponents[1]) else {
+            return nil
+        }
+        
+        return date
+    }
+    
+    var endDateIdentifier: Date? {
+        guard pathComponents.count > 2, let date = DateFormatter.yearMonthDay.date(from: pathComponents[2]) else {
+            return nil
+        }
+        
+        return date
+    }
+}
